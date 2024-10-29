c13dcab205f6d31f49f122b65ef4b3b5b3287bba
hwatkins-ch <hwatkins@companieshouse.gov.uk>
2024-10-29 14:15:00 +0000

    Rebase with master

--- src/test/java/uk/gov/companieshouse/lookup/controller/CompanyLookupControllerTest.java
+++ src/test/java/uk/gov/companieshouse/lookup/controller/CompanyLookupControllerTest.java
@@ -1,13 +1,20 @@
 package uk.gov.companieshouse.lookup.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,16 +22,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
-
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.lookup.internationalisation.InternationalisationConfig;
 import uk.gov.companieshouse.lookup.model.Company;
@@ -72,39 +72,40 @@ class CompanyLookupControllerTest {
     @DisplayName("Get Company Lookup - Success")
     void getCompanyLookup() throws Exception {
         this.mockMvc.perform(get(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM))
-            .andDo(print()).andExpect(status().isOk())
-            .andExpect(view().name(TEMPLATE))
-            .andExpect(model().attributeExists(MODEL_ATTRIBUTE));
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name(TEMPLATE))
+                .andExpect(model().attributeExists(MODEL_ATTRIBUTE));
     }
 
     @Test
     @DisplayName("Get Company Lookup - Failed, bad forward URL")
     void getCompanyLookupWhenForwardUrlBad() throws Exception {
         this.mockMvc.perform(get(COMPANY_LOOKUP_URL, "@:bad-forward-url"))
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(view().name(ERROR_TEMPLATE))
-            .andExpect(model().attributeDoesNotExist(MODEL_ATTRIBUTE));
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name(ERROR_TEMPLATE))
+                .andExpect(model().attributeDoesNotExist(MODEL_ATTRIBUTE));
     }
 
     @Test
     @DisplayName("Get Company Lookup Without Number - Success")
     void getCompanyLookupWithoutNumber() throws Exception {
         this.mockMvc.perform(get(COMPANY_LOOKUP_NO_NUMBER_URL, FORWARD_URL_PARAM))
-            .andDo(print())
-            .andExpect(status().is3xxRedirection())
-            .andExpect(view().name(UrlBasedViewResolver.REDIRECT_URL_PREFIX + FORWARD_URL_PARAM))
-            .andReturn();
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(
+                        view().name(UrlBasedViewResolver.REDIRECT_URL_PREFIX + FORWARD_URL_PARAM))
+                .andReturn();
     }
 
     @Test
     @DisplayName("Get Company Lookup Without Number - Failed, bad forward URL")
     void getCompanyLookupWithoutNumberWhenForwardUrlBad() throws Exception {
         this.mockMvc.perform(get(COMPANY_LOOKUP_NO_NUMBER_URL, "@:bad-forward-url"))
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(view().name(ERROR_TEMPLATE))
-            .andExpect(model().attributeDoesNotExist(MODEL_ATTRIBUTE));
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name(ERROR_TEMPLATE))
+                .andExpect(model().attributeDoesNotExist(MODEL_ATTRIBUTE));
     }
 
     @Test
@@ -112,20 +113,22 @@ class CompanyLookupControllerTest {
     void postCompanyLookup() throws Exception {
         when(companyLookupService.getCompanyProfile(COMPANY_NUMBER)).thenReturn(company);
         this.mockMvc
-            .perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM).param("companyNumber", COMPANY_NUMBER))
-            .andExpect(status().is3xxRedirection())
-            .andExpect(view().name(UrlBasedViewResolver.REDIRECT_URL_PREFIX + FORWARD_URL_PARAM))
-            .andReturn();
+                .perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM).param("companyNumber",
+                        COMPANY_NUMBER))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(
+                        view().name(UrlBasedViewResolver.REDIRECT_URL_PREFIX + FORWARD_URL_PARAM))
+                .andReturn();
     }
 
     @Test
     @DisplayName("Post Company Lookup - Fail bind error")
     void postCompanyLookupBindFail() throws Exception {
         this.mockMvc.perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-            .param(TEST_PATH, "test"))
-            .andExpect(status().isOk())
-            .andExpect(view().name(TEMPLATE))
-            .andExpect(model().attributeExists(MODEL_ATTRIBUTE));
+                        .param(TEST_PATH, "test"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(TEMPLATE))
+                .andExpect(model().attributeExists(MODEL_ATTRIBUTE));
     }
 
     @Test
@@ -133,9 +136,10 @@ class CompanyLookupControllerTest {
     void postCompanyLookupFail() throws Exception {
         when(companyLookupService.getCompanyProfile(COMPANY_NUMBER)).thenReturn(null);
         this.mockMvc
-            .perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM).param("companyNumber", COMPANY_NUMBER))
-            .andExpect(status().isOk())
-            .andExpect(view().name(TEMPLATE));
+                .perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM).param("companyNumber",
+                        COMPANY_NUMBER))
+                .andExpect(status().isOk())
+                .andExpect(view().name(TEMPLATE));
     }
 
     @Test
@@ -148,96 +152,102 @@ class CompanyLookupControllerTest {
 
     @Test
     @DisplayName("Test Company Lookup Controller with Language Parameter")
-    void testCompanyLookupController() throws Exception{
+    void testCompanyLookupController() throws Exception {
 
         MvcResult result = mockMvc.perform(get(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-        .param("lang", "cy"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andReturn();
+                        .param("lang", "cy"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
         String responseContent = result.getResponse().getContentAsString();
 
         Document doc = Jsoup.parse(responseContent);
-        
+
         assertThat(doc.selectFirst("title").text()).contains("Beth yw rhif y cwmni?");
         assertThat(doc.selectFirst("label").text()).contains("Beth yw rhif y cwmni?");
-        assertThat(doc.getElementById("company-lookup-hint").text()).contains("Cofnodwch y rif cwmni sy'n 8 nod");
-        assertThat(doc.getElementsByClass("govuk-inset-text").first().text()).contains("Os oes gennych rif cwmni sy'n 7 nod neu lai, nodwch seroau ar y dechrau fel ei fod yn 8 nod i gyd. Er enghraifft, os yw'n 12345, rhowch 00012345");
-        assertThat(doc.getElementById("company-number-help-text-link").text()).contains("Sut ydw i'n dod o hyd i rif y cwmni?");
-        assertThat(doc.getElementById("company-number-help-text").text()).contains("Gallwch ddod o hyd i hyn trwy chwilio am y cwmni ar gofrestr Tŷ'r Cwmnïau (yn agor mewn tab newydd).");
+        assertThat(doc.getElementById("company-lookup-hint").text()).contains(
+                "Cofnodwch y rif cwmni sy'n 8 nod");
+        assertThat(doc.getElementsByClass("govuk-inset-text").first().text()).contains(
+                "Os oes gennych rif cwmni sy'n 7 nod neu lai, nodwch seroau ar y dechrau fel ei fod yn 8 nod i gyd. Er enghraifft, os yw'n 12345, rhowch 00012345");
+        assertThat(doc.getElementById("company-number-help-text-link").text()).contains(
+                "Sut ydw i'n dod o hyd i rif y cwmni?");
+        assertThat(doc.getElementById("company-number-help-text").text()).contains(
+                "Gallwch ddod o hyd i hyn trwy chwilio am y cwmni ar gofrestr Tŷ'r Cwmnïau (yn agor mewn tab newydd).");
         assertThat(doc.select("input").get(1).val()).contains("Parhau");
     }
 
     @Test
     @DisplayName("Test Company Lookup Welsh Errors for null company number")
-    void testNullCompanyNumberErrorMessages() throws Exception{
+    void testNullCompanyNumberErrorMessages() throws Exception {
 
         MvcResult result = mockMvc.perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-        .param("lang", "cy"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andReturn();
+                        .param("lang", "cy"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
         String responseContent = result.getResponse().getContentAsString();
 
         Document doc = Jsoup.parse(responseContent);
-        
+
         assertThat(doc.selectFirst("#error-summary-heading").text()).contains("Mae yna broblem");
         assertTrue(doc.toString().contains("Cofnodwch rif y cwmni"));
     }
 
     @Test
     @DisplayName("Test Company Lookup Welsh Errors for no empty company number")
-    void testEmptyCompanyNumberErrorMessage() throws Exception{
+    void testEmptyCompanyNumberErrorMessage() throws Exception {
 
         MvcResult result = mockMvc.perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-        .param("lang", "cy").param("companyNumber", ""))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andReturn();
+                        .param("lang", "cy").param("companyNumber", ""))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
         String responseContent = result.getResponse().getContentAsString();
 
         Document doc = Jsoup.parse(responseContent);
-        
+
         assertThat(doc.selectFirst("#error-summary-heading").text()).contains("Mae yna broblem");
         assertTrue(doc.toString().contains("Cofnodwch rif y cwmni"));
     }
 
     @Test
     @DisplayName("Test Company Lookup Welsh Errors for wrong length company number")
-    void testCompanyNumberLengthErrorMessage() throws Exception{
+    void testCompanyNumberLengthErrorMessage() throws Exception {
 
         MvcResult result = mockMvc.perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-        .param("lang", "cy").param("companyNumber", "12"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andReturn();
+                        .param("lang", "cy").param("companyNumber", "12"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
         String responseContent = result.getResponse().getContentAsString();
 
         Document doc = Jsoup.parse(responseContent);
-        
+
         assertThat(doc.selectFirst("#error-summary-heading").text()).contains("Mae yna broblem");
-        assertThat(doc.selectFirst("#companyNumber-globalErrorId").text()).contains("Rhaid i rif y cwmni fod ag 8 nod. Os yw'n 7 nod neu lai, nodwch seroau ar y dechrau fel ei fod yn 8 nod i gyd.");
+        assertThat(doc.selectFirst("#companyNumber-globalErrorId").text()).contains(
+                "??Rhaid i rif y cwmni fod ag 8 nod. Os yw''n 7 nod neu lai, nodwch seroau ar y dechrau fel ei fod yn 8 nod i gyd._cy??");
     }
 
     @Test
     @DisplayName("Test Company Lookup Welsh Errors for invalid company number")
-    void testInvalidCompanyNumberErrorMessage() throws Exception{
+    void testInvalidCompanyNumberErrorMessage() throws Exception {
 
         MvcResult result = mockMvc.perform(post(COMPANY_LOOKUP_URL, FORWARD_URL_PARAM)
-        .param("lang", "cy").param("companyNumber", "san-goku"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andReturn();
+                        .param("lang", "cy").param("companyNumber", "san-goku"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
         String responseContent = result.getResponse().getContentAsString();
 
         Document doc = Jsoup.parse(responseContent);
-        
+
         assertThat(doc.selectFirst("#error-summary-heading").text()).contains("Mae yna broblem");
-        assertTrue(doc.toString().contains("Rhaid i rif cwmni gynnwys rhifau a llythrennau A i Z yn unig"));
+        assertTrue(doc.toString()
+                .contains("Rhaid i rif cwmni gynnwys rhifau a llythrennau A i Z yn unig"));
     }
 }
